b436e862e54dd6861316c0781e7387ae14496f3e
jsguti323 <99751636+jsguti323@users.noreply.github.com>
2022-06-09 11:49:15 -0700

    added t tests for each lot evrsus the overall population.

--- MechaCarChallenge.RScript.R
+++ MechaCarChallenge.RScript.R
@@ -2,7 +2,7 @@
 library(dplyr)
 
 # Read in mpg csv file as data frame.
-MechaCar_df <- read.csv(file = 'MechaCar_mpg.csv', check.names=F, stringsAsFactors =F)
+MechaCar_df <- read.csv(file ='MechaCar_mpg.csv', check.names=F, stringsAsFactors =F)
 
 # Perform linear regression.
 mpg_lm = lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=MechaCar_df)
@@ -20,6 +20,11 @@ coils_summary <- coils_tables  %>% summarize(Mean=mean(PSI), Median=median(PSI),
 # Group each manufacturing lot by summary statistics.
 lot_summary <- coils_tables %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI), .groups = 'keep')
 
+# Use T-Test to determine if statistical difference is significant for all lots versus the population.
+t.test(coils_tables$PSI, mu=1500)
 
-
+# Use T-Test to determine if statistical difference is significant for lot 1 versus the population.
+t.test(subset(coils_tables,Manufacturing_Lot=='Lot1')$PSI, mu=1500)
+t.test(subset(coils_tables,Manufacturing_Lot=='Lot2')$PSI, mu=1500)
+t.test(subset(coils_tables,Manufacturing_Lot=='Lot3')$PSI, mu=1500)
 
